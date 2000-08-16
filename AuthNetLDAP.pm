5233bc8c7c18b9b3e186653dbc1d28c95d3dc554
Mark Wilcox <mark@mjwilcox.com>
2000-08-16 15:41:47 +0000

    Import of MEWILCOX/apache.authnetldap.018 from CPAN.
    gitpan-cpan-distribution: apache.authnetldap
    gitpan-cpan-version:      .018
    gitpan-cpan-path:         MEWILCOX/apache.authnetldap.018.tar.gz
    gitpan-cpan-author:       MEWILCOX
    gitpan-cpan-maturity:     released

--- AuthNetLDAP.pm
+++ AuthNetLDAP.pm
@@ -1,54 +1,25 @@
 package Apache::AuthNetLDAP;
 
-
-#allow people to authenticate with LDAP server via Net::LDAP module
-#used Apache::AuthPerLDAP as basis
-
-
 use strict;
-use Carp;
-use vars qw($VERSION @ISA @EXPORT @EXPORT_OK $AUTOLOAD);
-
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK );
 use Net::LDAP;
 use mod_perl;
 use Apache::Constants qw(OK AUTH_REQUIRED);
 
 
-require Exporter;
-require DynaLoader;
-require AutoLoader;
 
-@ISA = qw(Exporter DynaLoader);
+require Exporter;
+
+
+@ISA = qw(Exporter AutoLoader);
 # Items to export into callers namespace by default. Note: do not export
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
 @EXPORT = qw(
 	
 );
-$VERSION = '0.16';
+$VERSION = '0.18';
 
-sub AUTOLOAD {
-    # This AUTOLOAD is used to 'autoload' constants from the constant()
-    # XS function.  If a constant is not found then control is passed
-    # to the AUTOLOAD in AutoLoader.
-
-    my $constname;
-    ($constname = $AUTOLOAD) =~ s/.*:://;
-    my $val = constant($constname, @_ ? $_[0] : 0);
-    if ($! != 0) {
-	if ($! =~ /Invalid/) {
-	    $AutoLoader::AUTOLOAD = $AUTOLOAD;
-	    goto &AutoLoader::AUTOLOAD;
-	}
-	else {
-		croak "Your vendor has not defined Apache::AuthNetLDAP macro $constname";
-	}
-    }
-    eval "sub $AUTOLOAD { $val }";
-    goto &$AUTOLOAD;
-}
-
-bootstrap Apache::AuthNetLDAP $VERSION;
 
 # Preloaded methods go here.
 
@@ -133,8 +104,6 @@ sub handler
 }
 # Autoload methods go after =cut, and are processed by the autosplit program.
 
-1;
-__END__
 # Below is the stub of documentation for your module. You better edit it!
 
 =head1 NAME
@@ -230,7 +199,7 @@ Then in your httpd.conf file or .htaccess file, in either a <Directory> or <Loca
 
 =head1 HOMEPAGE
 
-	Module Home:http://courses.unt.edu/mewilcox/
+	Module Home:http://perl-ldap.sourceforge.net/
 
 =head1 AUTHOR
    	Mark Wilcox mewilcox@unt.edu
@@ -251,3 +220,7 @@ You can distribute and modify in accordance to the same license as Perl. Though 
 Like most of the stuff on the 'net, I got this copy to work for me without destroying mankind, you're mileage may vary.
 
 =cut
+
+
+1;
+__END__
